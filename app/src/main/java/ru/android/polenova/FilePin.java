a0a45c303549f2445c39fbbe3d6de32b629348d6
Marina <56669936+Polenova@users.noreply.github.com>
2020-01-24 14:22:47 +0100

    Update FilePin.java

--- app/src/main/java/ru/android/polenova/FilePin.java
+++ app/src/main/java/ru/android/polenova/FilePin.java
@@ -1,7 +1,12 @@
 package ru.android.polenova;
 
 import android.content.Context;
+import android.nfc.Tag;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -10,11 +15,50 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class FilePin implements Keystore{
+public class FilePin implements Keystore {
 
     private static final String FILE_PASSWORD = "password_notes";
+    private Context context;
 
-    static boolean exportPIN(Context context, String stringPin) {
+    public FilePin(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public boolean hasPin() {
+        return !TextUtils.isEmpty(readPin());
+    }
+
+    @Nullable
+    private String readPin() {
+        String stringPassword;
+        try (FileInputStream fileInputStream = context.openFileInput(FILE_PASSWORD);
+             InputStreamReader streamReader = new InputStreamReader(fileInputStream);
+             BufferedReader bufferedReader = new BufferedReader(streamReader)) {
+            stringPassword = bufferedReader.readLine();
+            return stringPassword;
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean checkPin(String pin) {
+        return pin.equals(readPin());
+    }
+
+    @Override
+    public void saveNew(String pin) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(context.openFileOutput(FILE_PASSWORD, Context.MODE_PRIVATE)))) {
+            bufferedWriter.write(pin);
+            bufferedWriter.close();
+            Toast.makeText(context, "пароль сохранен", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(context, "пароль  не сохранен", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*static boolean exportPIN(Context context, String stringPin) {
         String stringPassword = stringPin.toString();
         BufferedWriter bufferedWriter = null;
         try {
@@ -58,20 +102,5 @@ public class FilePin implements Keystore{
             }
         }
         return null;
-    }
-
-    @Override
-    public boolean hasPin() {
-        return false;
-    }
-
-    @Override
-    public boolean checkPin(String pin) {
-        return false;
-    }
-
-    @Override
-    public void saveNew(String pin) {
-
-    }
+    }*/
 }
